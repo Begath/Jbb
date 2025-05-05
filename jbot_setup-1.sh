6ca4b913d50f470c73002f0007b032b54d6f1c80
Begath <Joeb955@hotmail.co.uk>
2025-05-05 21:54:39 +0100

    Update jbot_setup-1.sh

--- jbot_setup-1.sh
+++ jbot_setup-1.sh
@@ -5,6 +5,6 @@ pkg install -y git curl
 GH_USER="Begath"
 GH_TOKEN="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"
 
-git clone https://${GH_USER}:${GH_TOKEN}@github.com/jbotdev/jbot.git
-cd jbot || exit 1
+git clone https://$GH_USER:$GH_TOKEN@github.com/Begath/Jbb.git
+cd Jbb || exit 1
 bash setup.sh
